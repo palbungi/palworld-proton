3b88c305d5e725ef4596dc5506482245ccc8c8c0
팰붕이 <107607815+palbungi@users.noreply.github.com>
2025-08-07 15:47:56 +0900

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# cron 실행 (실패해도 계속 진행)
-service cron start || echo "cron failed to start"
-# chown 실패 무시
-chown -R 1001:1002 /app || echo "chown failed, continuing anyway"
-# UID:GID 1001:1002로 start.sh 실행
+service cron start
+chown -R 1001:1002 /app || true
 exec gosu 1001:1002 /app/start.sh
